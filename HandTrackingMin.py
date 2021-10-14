49a317ff7c53f191387ff1d925b30d0cf890aa61
ThejakaSEP <thejaka.mahaulpatha@gmail.com>
2021-10-14 22:54:12 +0530

    Add files via upload

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -8,6 +8,9 @@ mpHands  = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
@@ -17,7 +20,16 @@ while True:
     #Extracting each hand
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img,handLms)
+            #Get the information of hands. Landmarks information(x&Y coordinates) and ID number
+            for id,lm in enumerate(handLms.landmark):
+                print(id,'\n',lm)
+            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
 
 
     cv2.imshow("image",img)
