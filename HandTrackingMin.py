966325d150a37262398490068f90ba41fb501fca
ThejakaSEP <thejaka.mahaulpatha@gmail.com>
2021-10-14 22:39:21 +0530

    Add files via upload

---
+++ HandTrackingMin.py
@@ -0,0 +1,25 @@
+import cv2
+import mediapipe as mp
+import time # to check the frame rate
+
+cap = cv2.VideoCapture(0)
+
+mpHands  = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks) # to Check whether it detects a hand
+
+    #Extracting each hand
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img,handLms)
+
+
+    cv2.imshow("image",img)
+    cv2.waitKey(1)
+
