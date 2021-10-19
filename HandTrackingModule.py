c790e2955fa60fb68332ec513a74d1964e847395
ThejakaSEP <thejaka.mahaulpatha@gmail.com>
2021-10-19 12:11:24 +0530

    Add files via upload

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,3 +1,4 @@
+#This module is to get the positions of coordinates easily
 import cv2
 import mediapipe as mp
 import time # to check the frame rate
@@ -14,41 +15,56 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils
 
 
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    #print(results.multi_hand_landmarks) # to Check whether it detects a hand
+    def findHands(self,img,draw=True):
+        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        #print(results.multi_hand_landmarks) # to Check whether it detects a hand
 
-    #Extracting each hand
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            #Get the information of hands. Landmarks information(x&Y coordinates) and ID number
-            for id,lm in enumerate(handLms.landmark):
-                #print(id,'\n',lm)
-                h,w,c = img.shape
-                cx,cy = int(lm.x*w),int(lm.y*h)
-                print(id,cx,cy) #Get the pixel location
-                # if id==0:
-                cv2.circle(img,(cx,cy),10,(255,0,0),cv2.FILLED) #Drawing a Circle on the ID 0 location
-            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+        #Extracting each hand
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img,handLms,self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self,img,handNo=0,draw=True):
+        # Get the information of hands. Landmarks information(x&Y coordinates) and ID number
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id,'\n',lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)  # Get the pixel location
+                lmList.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 25, (255, 0, 0), cv2.FILLED)  # Drawing a Circle on the ID 0 location
+
+        return lmList
 
 
 def main():
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
+    detector = handDetector()
     while True:
         success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) !=0:
+            print(lmList[4]) #4 is the tip of the thumb
 
-    cTime = time.time()
-    fps = 1/(cTime - pTime)
-    pTime = cTime
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
 
-    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+        cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
 
 
-    cv2.imshow("image",img)
-    cv2.waitKey(1)
+        cv2.imshow("image",img)
+        cv2.waitKey(1)
 
 
 if __name__ == "__main__":
